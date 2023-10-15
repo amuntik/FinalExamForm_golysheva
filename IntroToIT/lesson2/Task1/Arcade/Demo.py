21922a8b835173bdf9449e3dee57b015f72b03b1
Julia <assolfernandes6@gmail.com>
2023-10-15 14:48:46 +0300

    style: поменяла персонажей

--- IntroToIT/lesson2/Task1/Arcade/Demo.py
+++ IntroToIT/lesson2/Task1/Arcade/Demo.py
@@ -16,10 +16,10 @@ shift = 0
 x_start, y_start = 20, 10
 
 img_file_back =     path.dirname(__file__)+ '/cave.png'
-img_file_hero =     path.dirname(__file__)+ '/m1.png'
-img_file_enemy =    path.dirname(__file__)+ '/enemy.png' 
+img_file_hero =     path.dirname(__file__)+ '/tanos.png'
+img_file_enemy =    path.dirname(__file__)+ '/halk.png' 
 img_file_bomb =     path.dirname(__file__)+ '/bomb.png'
-img_file_princess = path.dirname(__file__)+ '/princess.png'
+img_file_princess = path.dirname(__file__)+ '/spiderman.png'
 FPS = 60
 
 # цвета:
